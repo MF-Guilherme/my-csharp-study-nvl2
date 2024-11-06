a1ba4c0ceca94862428328f1ca3e57150930ebf2
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-11-05 21:07:00 -0300

    Retorno simples com Body Expressions

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -11,10 +11,8 @@ namespace ExemploExplorando.Models
 
         public string Nome 
         { 
-            get 
-            {
-                return _nome.ToUpper();
-            } 
+            get => _nome.ToUpper();
+            
             set 
             {
                  if (value == "") //precisa ser a variavel "value"
